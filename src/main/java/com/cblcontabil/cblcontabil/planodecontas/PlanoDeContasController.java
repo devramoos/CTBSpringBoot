85e804b72f546b2fece5970fa8aab55b98e42a5f
devramoos <rodrigojgramos@outlook.com>
2026-01-20 14:52:40 -0300

    Feat: Inserindo o FlyWay. Fix: Coloca os metódos service e mapea as rotas no controller.

--- src/main/java/com/cblcontabil/cblcontabil/planodecontas/PlanoDeContasController.java
+++ src/main/java/com/cblcontabil/cblcontabil/planodecontas/PlanoDeContasController.java
@@ -1,4 +1,48 @@
 package com.cblcontabil.cblcontabil.planodecontas;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/planodecontas")
 public class PlanoDeContasController {
+
+    private PlanoDeContasService planoDeContasService;
+
+    public PlanoDeContasController(PlanoDeContasService planoDeContasService) {
+        this.planoDeContasService = planoDeContasService;
+    }
+
+    @GetMapping
+    public String boasVindas(){
+        return "Boas vindas";
+    }
+
+    @PostMapping("/criar")
+    public String criarContas(){
+        return "Nova conta criada";
+    }
+
+    @GetMapping("/listar")
+    public List<PlanoDeContasModel> listarConta(){
+        return planoDeContasService.listarConta();
+    }
+
+    @GetMapping("/listaporid")
+    public String listarContaPorCodigo(){
+        return "Conta por id mostrada com sucesso";
+    }
+
+    @PutMapping("/alterarConta")
+    public String alterarConta(){
+        return "Sua conta foi alterada com sucesso";
+    }
+
+    @DeleteMapping("/deletarConta")
+    public String deletarConta(){
+        return "Deletado sua conta";
+    }
+
 }
--- src/main/java/com/cblcontabil/cblcontabil/planodecontas/PlanoDeContasService.java
+++ src/main/java/com/cblcontabil/cblcontabil/planodecontas/PlanoDeContasService.java
@@ -1,4 +1,19 @@
 package com.cblcontabil.cblcontabil.planodecontas;
 
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class PlanoDeContasService {
+
+    private PlanoDeContasRepository planoDeContasRepository;
+
+    public PlanoDeContasService(PlanoDeContasRepository planoDeContasRepository) {
+        this.planoDeContasRepository = planoDeContasRepository;
+    }
+
+    public List<PlanoDeContasModel> listarConta(){
+        return planoDeContasRepository.findAll();
+    }
 }
